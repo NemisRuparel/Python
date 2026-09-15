a5aab96e7c7aa98f171da07e1a92148064819d20
Nemis Ruparel <nemisruparel07@gmail.com>
2026-09-15 21:14:47 +0530

    Add Comments.py to display student information

---
+++ Basics/Comments.py
@@ -0,0 +1,10 @@
+# Program to display student information
+
+name = "Nemis"
+age = 20
+marks = 85.5
+
+# Display student information
+print("Name:", name)
+print("Age:", age)
+print("Marks:", marks)
