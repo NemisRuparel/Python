14885efd65b2a07d045dc8a2a8d06f23f6b7d84f
Nemis Ruparel <nemisruparel07@gmail.com>
2026-09-15 21:13:29 +0530

    Add Variables.py with variable examples and print statements

---
+++ Basics/Variables.py
@@ -0,0 +1,13 @@
+# Variables
+
+name = "Nemis"
+age = 20
+marks = 85.5
+grade = "A"
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Marks:", marks)
+print("Grade:", grade)
+print("Student:", is_student)
