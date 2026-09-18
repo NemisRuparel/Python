4180cb646c252d1488fa57a6007db4aeac82a0f5
Nemis Ruparel <nemisruparel07@gmail.com>
2026-09-18 12:08:54 +0530

    Enhance InputOutput.py with improved print statements and formatting for user input

--- 01_Basics/InputOutput.py
+++ 01_Basics/InputOutput.py
@@ -1,7 +1,19 @@
 # Input and Output
 
+# Taking inputs
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 
-print("Hello", name)
-print("Your age is", age)
+# Normal Print
+print("\nHello", name)
+print("Your age is", age,end="\n\n")
+print("Hello " + name + ", you are " + str(age) + " years old.",end="\n\n")
+
+# printing " " inside print statement 
+print("Hello \"World\"")
+print("Hello 'World'")
+print('Hello "World"',end='\n\n')
+
+#format-String print
+print(f"Hello {name}, you are {age} years old.")
+
