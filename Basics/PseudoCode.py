a6859c12b698285bfbe9b7d0b86127239048c140
Nemis Ruparel <nemisruparel07@gmail.com>
2026-09-18 10:32:46 +0530

    Add PseudoCode.py for basic addition example with user input

---
+++ Basics/PseudoCode.py
@@ -0,0 +1,17 @@
+# START
+
+# INPUT first number
+# INPUT second number
+
+# sum = first number + second number
+
+# DISPLAY sum
+
+# END
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+sum = a + b
+
+print("Sum =", sum)
